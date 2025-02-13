af90e6233adf91262210ff765dcbb312fb18ff4c
Alberto <amozosk@gmail.com>
2025-02-13 10:09:03 +0100

    Modificado el collate de las tablas sql para que las comparaciones y consultas sean case sensitive. Añadida como clave de la tabla de friend_requests ambos usernames

--- database_scripts/friends.sql
+++ database_scripts/friends.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `friends`;
 CREATE TABLE `friends` (
   `username1` varchar(45) DEFAULT NULL,
   `username2` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 LOCK TABLES `friends` WRITE;
 
